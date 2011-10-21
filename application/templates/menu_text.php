b058abca7741c959fd0d02c082c58ca2731275e9
irakli <irakli.darbuashvili@jumpstart.ge>
2011-10-21 18:08:12 +0400

    Lang

--- application/templates/menu_text.php
+++ application/templates/menu_text.php
@@ -1,11 +1,11 @@
 <div class="page-container">
 
 	<?php if (empty($menu) OR empty($menu['title'])): ?>
-		<h1 style="font-size: 18px;">Under construction</h1>
+		<h1 style="font-size: 18px;"><?php echo l('mt_under_construction') ?></h1>
 	<?php else: ?>
 		<h1 style="font-size: 18px;">
 			<?php echo $menu['title'] ?>
-			<?php userloggedin() AND print("<a class='region_link' style='float: right; display: block; font-size: 12px;' href='" . href('admin/menu/' . $menu['unique'], TRUE) . "'>Edit</a>"); ?>
+			<?php userloggedin() AND print("<a class='region_link' style='float: right; display: block; font-size: 12px;' href='" . href('admin/menu/' . $menu['unique'], TRUE) . "'>" . l('mt_edit') . "</a>"); ?>
 		</h1>
 		<span style="font-size: 15px; text-align: justify"><?php echo $menu['text']; ?></span>
 	<?php endif; ?>
