444c662442f4f92ac0835c6e6c430e828c3fdedd
Otar Chekurishvili <chekura@gmail.com>
2011-10-21 18:20:17 +0400

    Don't display news switcher on the home page if we don't have enough news.

--- application/templates/home.php
+++ application/templates/home.php
@@ -65,28 +65,30 @@
                     <img src="<?php $count > 2 AND print $third_src ?>" />
                 </div>
             <?php endif; ?>
-            <div id="right_titles_box">
-                <?php
-                for ($index = 2; $index < 6; $index++):
-                    if (!isset($news_all[$index]) OR empty($news_all[$index]))
-                    {
-                        break;
-                    }
-                    $news = $news_all[$index];
-                    $src = substr($news['image'], 0, 7);
-                    $src = ($src == "http://" OR $src == "https:/") ? $news['image'] : href() . $news['image'];
-                    ?>
-                    <div class="right_title_one">
-                        <a href="<?php echo href("news/" . $news['unique'], TRUE); ?>">
-                            <span><</span><?php echo strtoupper($news['title']); ?>
-                        </a>
-                        <input type="hidden" class="src_container" value="<?php echo $src; ?>" />
-                    </div>
-                    <?php if ($index != 5): ?>
-                        <hr style="background: transparent; border: 0px; height: 2px;" />
-                    <?php endif; ?>
-                <?php endfor; ?>
-            </div>
+            <?php if (isset($news_all[2]) AND $news_all[5]): ?>
+                <div id="right_titles_box">
+                    <?php
+                    for ($index = 2; $index < 6; $index++):
+                        if (!isset($news_all[$index]) OR empty($news_all[$index]))
+                        {
+                            break;
+                        }
+                        $news = $news_all[$index];
+                        $src = substr($news['image'], 0, 7);
+                        $src = ($src == "http://" OR $src == "https:/") ? $news['image'] : href() . $news['image'];
+                        ?>
+                        <div class="right_title_one">
+                            <a href="<?php echo href("news/" . $news['unique'], TRUE); ?>">
+                                <span><</span><?php echo strtoupper($news['title']); ?>
+                            </a>
+                            <input type="hidden" class="src_container" value="<?php echo $src; ?>" />
+                        </div>
+                        <?php if ($index != 5): ?>
+                            <hr style="background: transparent; border: 0px; height: 2px;" />
+                        <?php endif; ?>
+                    <?php endfor; ?>
+                </div>
+            <?php endif; ?>
         </div>
 
         <div id="news_bottom_content">
