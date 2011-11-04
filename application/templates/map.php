15e491167eb3a328efd344261bb949582bcacc65
Otar Chekurishvili <chekura@gmail.com>
2011-11-04 22:26:10 +0400

    Mapping done!

--- application/templates/map.php
+++ application/templates/map.php
@@ -2,7 +2,7 @@
 
     <div id="map" style="height: 360px; width: 100%"></div>
 
-    <div id="map-overlay" style="display: none"><?php echo strtoupper(l('map_active')) ?></div>
+    <div id="map-overlay"><?php echo strtoupper(l('map_active')) ?></div>
 
     <div id="tooltip"><span></span></div>
 
@@ -12,67 +12,42 @@
         <li id="map-plus"><a class="map-button" href="javascript:zoom_in();">+</a></li>
 
         <li id="map-filter">
-            <a class="map-button" href="javascript:;"><img src="images/map/filter.png" width="13" height="11" alt="" /><?php echo l('map_projects') ?></a>
-            <ul>
-                <li class="first">
-                    <a href="javascript:;" class="skip"><img src="images/map/projects/sewage_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_sewage') ?></a>
-                    <ul>
-                        <li><a class="sub" href="javascript:toggle_projects('sewage', 'completed');"><img src="images/map/projects/sewage_completed.png" width="27" height="27" alt="" /><?php echo l('map_type_completed') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('sewage', 'current');"><img src="images/map/projects/sewage_current.png" width="27" height="27" alt="" /><?php echo l('map_type_current') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('sewage', 'scheduled');"><img src="images/map/projects/sewage_scheduled.png" width="27" height="27" alt="" /><?php echo l('map_type_scheduled') ?></a></li>
-                    </ul>
+            <a id="control-projects" class="map-button" href="javascript:;"><img src="images/map/filter.png" width="13" height="11" alt="" /><?php echo l('map_projects') ?></a>
+            <ul><?php
+$idx = 0;
+$types = array('sewage', 'water_supply', 'water_pollution', 'irrigation', 'water_quality', 'water_accidents');
+$statuses = array('completed', 'current', 'scheduled');
+foreach ($types AS $type):
+    $check_type = json_decode(file_get_contents(URL . "map-data/projects/{$type}?lang=" . LANG));
+    if (empty($check_type))
+        continue;
+   ?>
+                <li<?php $idx == 0 AND print ' class="first"' ?>>
+                    <a id="control-<?php echo $type ?>" href="javascript:;" class="skip"><img src="images/map/projects/<?php echo $type ?>_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_' . $type) ?></a>
+                    <ul><?php
+                foreach ($statuses AS $status):
+                    $check_type_status = json_decode(file_get_contents(URL . "map-data/projects/{$type}/{$status}?lang=" . LANG));
+                    if (empty($check_type_status))
+                        continue;
+                    echo '<li><a id="control-' . $type . '-' . $status . '" class="sub" href="javascript:toggle_projects(\'' . $type . '\', \'' . $status . '\');"><img src="images/map/projects/' . $type . '_' . $status . '.png" width="27" height="27" alt="" />' . l('map_type_' . $status) . '</a></li>';
+                endforeach;
+   ?></ul>
                 </li>
-                <li>
-                    <a href="javascript:;" class="skip"><img src="images/map/projects/water_supply_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_water_supply') ?></a>
-                    <ul>
-                        <li><a class="sub" href="javascript:toggle_projects('water_supply', 'completed');"><img src="images/map/projects/water_supply_completed.png" width="27" height="27" alt="" /><?php echo l('map_type_completed') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_supply', 'current');"><img src="images/map/projects/water_supply_current.png" width="27" height="27" alt="" /><?php echo l('map_type_current') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_supply', 'scheduled');"><img src="images/map/projects/water_supply_scheduled.png" width="27" height="27" alt="" /><?php echo l('map_type_scheduled') ?></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;" class="skip"><img src="images/map/projects/water_pollution_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_water_pollution') ?></a>
-                    <ul>
-                        <li><a class="sub" href="javascript:toggle_projects('water_pollution', 'completed');"><img src="images/map/projects/water_pollution_completed.png" width="27" height="27" alt="" /><?php echo l('map_type_completed') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_pollution', 'current');"><img src="images/map/projects/water_pollution_current.png" width="27" height="27" alt="" /><?php echo l('map_type_current') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_pollution', 'scheduled');"><img src="images/map/projects/water_pollution_scheduled.png" width="27" height="27" alt="" /><?php echo l('map_type_scheduled') ?></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;" class="skip"><img src="images/map/projects/irrigation_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_irrigation') ?></a>
-                    <ul>
-                        <li><a class="sub" href="javascript:toggle_projects('irrigation', 'completed');"><img src="images/map/projects/irrigation_completed.png" width="27" height="27" alt="" /><?php echo l('map_type_completed') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('irrigation', 'current');"><img src="images/map/projects/irrigation_current.png" width="27" height="27" alt="" /><?php echo l('map_type_current') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('irrigation', 'scheduled');"><img src="images/map/projects/irrigation_scheduled.png" width="27" height="27" alt="" /><?php echo l('map_type_scheduled') ?></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;" class="skip"><img src="images/map/projects/water_quality_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_water_quality') ?></a>
-                    <ul>
-                        <li><a class="sub" href="javascript:toggle_projects('water_quality', 'completed');"><img src="images/map/projects/water_quality_completed.png" width="27" height="27" alt="" /><?php echo l('map_type_completed') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_quality', 'current');"><img src="images/map/projects/water_quality_current.png" width="27" height="27" alt="" /><?php echo l('map_type_current') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_quality', 'scheduled');"><img src="images/map/projects/water_quality_scheduled.png" width="27" height="27" alt="" /><?php echo l('map_type_scheduled') ?></a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="javascript:;" class="skip"><img src="images/map/projects/water_accidents_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_water_accidents') ?></a>
-                    <ul>
-                        <li><a class="sub" href="javascript:toggle_projects('water_accidents', 'completed');"><img src="images/map/projects/water_accidents_completed.png" width="27" height="27" alt="" /><?php echo l('map_type_completed') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_accidents', 'current');"><img src="images/map/projects/water_accidents_current.png" width="27" height="27" alt="" /><?php echo l('map_type_current') ?></a></li>
-                        <li><a class="sub" href="javascript:toggle_projects('water_accidents', 'scheduled');"><img src="images/map/projects/water_accidents_scheduled.png" width="27" height="27" alt="" /><?php echo l('map_type_scheduled') ?></a></li>
-                    </ul>
-                </li>
+                    <?php
+                    $idx++;
+                endforeach;
+                ?>
             </ul>
         </li>
 
         <li id="map-overlays">
             <a class="map-button"><img src="images/map/layer.png" width="13" height="11" alt="" /><?php echo l('map_overlays') ?></a>
             <ul>
-                <li class="first"><a class="active" href="javascript:toggle_overlay('cities');toggle_overlay('urban');toggle_overlay('villages');"><img src="images/map/city.png" width="15" height="14" alt="" /><?php echo l('map_settlements') ?></a></li>
-                <li><a href="javascript:toggle_overlay('hydro');"><img src="images/map/hydro.png" width="15" height="14" alt="" /><?php echo l('map_hydro') ?></a></li>
-                <li><a class="active" href="javascript:toggle_overlay('water');"><img src="images/map/waters.png" width="15" height="14" alt="" /><?php echo l('map_water') ?></a></li>
-                <li><a class="active" href="javascript:toggle_overlay('protected_areas');"><img src="images/map/protected_areas.png" width="15" height="14" alt="" /><?php echo l('map_protected_areas') ?></a></li>
-                <li><a href="javascript:toggle_overlay('roads');"><img src="images/map/roads.png" width="15" height="14" alt="" /><?php echo l('map_roads') ?></a></li>
+                <li class="first"><a id="overlay-cities" class="active" href="javascript:toggle_overlay('cities');toggle_overlay('urban');toggle_overlay('villages');"><img src="images/map/city.png" width="15" height="14" alt="" /><?php echo l('map_settlements') ?></a></li>
+                <li><a id="overlay-hydro" href="javascript:toggle_overlay('hydro');"><img src="images/map/hydro.png" width="15" height="14" alt="" /><?php echo l('map_hydro') ?></a></li>
+                <li><a id="overlay-water" href="javascript:toggle_overlay('water');"><img src="images/map/waters.png" width="15" height="14" alt="" /><?php echo l('map_water') ?></a></li>
+                <li><a id="overlay-protected_areas" class="active" href="javascript:toggle_overlay('protected_areas');"><img src="images/map/protected_areas.png" width="15" height="14" alt="" /><?php echo l('map_protected_areas') ?></a></li>
+                <li><a id="overlay-roads" href="javascript:toggle_overlay('roads');"><img src="images/map/roads.png" width="15" height="14" alt="" /><?php echo l('map_roads') ?></a></li>
             </ul>
         </li>
 
