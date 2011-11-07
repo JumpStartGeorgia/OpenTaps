2c96caca5a4c623373394c60ebe91a5d8fcc090e
Otar Chekurishvili <chekura@gmail.com>
2011-11-07 12:09:29 +0400

    Temporary disabled map projects menu validation.

--- application/templates/map.php
+++ application/templates/map.php
@@ -18,17 +18,17 @@ $idx = 0;
 $types = array('sewage', 'water_supply', 'water_pollution', 'irrigation', 'water_quality', 'water_accidents');
 $statuses = array('completed', 'current', 'scheduled');
 foreach ($types AS $type):
-    $check_type = json_decode(file_get_contents(URL . "map-data/projects/{$type}?lang=" . LANG));
-    if (empty($check_type))
-        continue;
+    //$check_type = json_decode(file_get_contents(URL . "map-data/projects/{$type}?lang=" . LANG));
+    //if (empty($check_type))
+    //continue;
    ?>
                 <li<?php $idx == 0 AND print ' class="first"' ?>>
                     <a id="control-<?php echo $type ?>" href="javascript:;" class="skip"><img src="images/map/projects/<?php echo $type ?>_completed.png" width="27" height="27" alt="" /><?php echo l('map_project_' . $type) ?></a>
                     <ul><?php
                 foreach ($statuses AS $status):
-                    $check_type_status = json_decode(file_get_contents(URL . "map-data/projects/{$type}/{$status}?lang=" . LANG));
-                    if (empty($check_type_status))
-                        continue;
+                    //$check_type_status = json_decode(file_get_contents(URL . "map-data/projects/{$type}/{$status}?lang=" . LANG));
+                    //if (empty($check_type_status))
+                    //continue;
                     echo '<li><a id="control-' . $type . '-' . $status . '" class="sub" href="javascript:toggle_projects(\'' . $type . '\', \'' . $status . '\');"><img src="images/map/projects/' . $type . '_' . $status . '.png" width="27" height="27" alt="" />' . l('map_type_' . $status) . '</a></li>';
                 endforeach;
    ?></ul>
