4a8d7be93f1c18bb486b000feab1bfa037e50d78
Otar Chekurishvili <chekura@gmail.com>
2011-11-14 16:24:43 +0400

    Fixed news tags links markup.

--- application/templates/news.php
+++ application/templates/news.php
@@ -14,54 +14,45 @@ $this_type = empty($this_type) ? NULL : 'type/' . $this_type . '/';
 <a name="news"></a>
 
 <div id='tag_content'>
+
     <div id='left_list'>
-        <div class='group headers'>
-            <div class='headers_left'><?php echo strtoupper(l('news')) ?></div>
-            <?php /* <div class='headers_right' style='cursor: pointer;' onclick='$("#newstype_filter").slideToggle("fast");'>SORT BY ▾</div> */ ?>
-        </div>
+
+        <div class='group headers'><div class='headers_left'><?php echo strtoupper(l('news')) ?></div></div>
 
         <div class='group' id='newstype_filter' style='width: 100%; border-bottom: 1px solid #eee;'>
+
             <div class='titletype_left' style='padding-left: 11px; font-size:9px;'>
-                <a href='<?php echo href('news', TRUE, "news") ?>'
-                   class='choosedef<?php empty($this_type_original) AND print("_selected") ?>'>
-                       <?php echo l('news_page_all'); ?>
-                </a>
+
+                <a href='<?php echo href('news', TRUE, "news") ?>' class='choosedef<?php echo empty($this_type_original) ? '_selected' : NULL ?>'><?php echo l('news_page_all'); ?></a>
                 <?php
                 $types = config("news_types");
                 foreach ($types as $type):
                     ?>
-                    <a href='<?php echo href('news/type/' . $type, TRUE, "news") ?>'
-                       class='choosedef<?php ($type == $this_type_original) AND print("_selected") ?>'>
-                           <?php echo strtoupper(l('nt_' . $type)); ?>
-                    </a>
+                    <a href='<?php echo href('news/type/' . $type, TRUE, "news") ?>' class='choosedef<?php echo $type == $this_type_original ? '_selected' : NULL ?>'><?php echo strtoupper(l('nt_' . $type)); ?></a>
                 <?php endforeach; ?>
             </div>
 
-            <div class='titletype_right' style='margin-top:4px;'>
-                <?php /* <span style='color: rgba(86, 86, 86, .6)'>Type of news: </span>
-                  <div class='newstypebg1'></div> Project
-                  <div class='newstypebg2'></div> Media
-                  <div class='newstypebg3'></div> Pro Media */ ?>
-            </div>
+            <div class='titletype_right' style='margin-top:4px;'></div>
+
         </div>
 
         <div id='internal_container' class='group'>
+
             <?php foreach ($news_all as $index => $news): ?>
-                <div class='content_each group <?php ($index % 2 == 1) AND print("with_bg"); ?>'>
-                    <a href="<?php echo href('news/' . $news['unique'], TRUE, "news") ?>">
-                        <div class='content_each_left'>
-                            <div class='content_each_title'><?php echo $news['title'] ?></div>
-                            <div class='content_each_body'>
-                                <?php echo word_limiter(strip_tags($news['body']), 170); ?>
-                            </div>
+                <div class='content_each group <?php echo ($index % 2 == 1) ? 'with_bg' : NULL ?>'>
+                    <div class='content_each_left'>
+                        <div class='content_each_title'><?php echo $news['title'] ?></div>
+                        <div class='content_each_body'>
+                            <?php echo word_limiter(strip_tags($news['body']), 170); ?>
                         </div>
-                        <?php $rgba = 1 - ($index % 3) / 2.5; ?>
-                        <div class='content_each_right' style='border-left: 7px solid rgba(12, 181, 245, <?php echo $rgba; ?>)'>
-                            <div style='padding: 4px; padding-top: 15px; font-size: 10px; text-align: center;'>
-                                <img src="<?php echo href() . $images[$news['category']] ?>" height='36px' /> <br />
-                                <?php echo dateformat($news['published_at']); ?>
-                            </div>
+                    </div>
+                    <?php $rgba = 1 - ($index % 3) / 2.5; ?>
+                    <div class='content_each_right' style='border-left: 7px solid rgba(12, 181, 245, <?php echo $rgba; ?>)'>
+                        <div style='padding: 4px; padding-top: 15px; font-size: 10px; text-align: center;'>
+                            <img src="<?php echo href() . $images[$news['category']] ?>" height='36px' /> <br />
+                            <?php echo dateformat($news['published_at']); ?>
                         </div>
+                    </div>
                 </div>
             <?php endforeach; ?>
 
@@ -79,13 +70,15 @@ $this_type = empty($this_type) ? NULL : 'type/' . $this_type . '/';
                     if ($page != $current_page):
                         ?>
                         <a href='<?php echo href("news/" . $this_type . $pagelink, TRUE, "news") ?>'>
-                            <?php echo $page;
-                            ($total_pages == $page) OR print(" |"); ?>
+                            <?php
+                            echo $page;
+                            echo ($total_pages == $page) ? NULL : " |";
+                            ?>
                         </a>
                         <?php
                     else:
                         echo $page;
-                        ($total_pages == $page) OR print(" |");
+                        echo ($total_pages == $page) ? NULL : " |";
                     endif;
                 endfor;
                 if ($current_page < $total_pages):
